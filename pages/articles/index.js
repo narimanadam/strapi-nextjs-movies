8b49f0b2f6fd3124743462de5f6dff44460fe34b
narimanadam <narimanadam7@gmail.com>
2021-07-17 23:18:23 +0400

    update article image

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -2,14 +2,13 @@ import { ArticleCard } from "@movies-app/components";
 import React from "react";
 
 const Blog = ({ articles }) => {
-  const { API_URL } = process.env;
   return (
     <div className="container mx-auto mt-8">
       <div className="grid grid-cols-3 gap-5">
         {articles.map(({ id, articleImage, title, author, date }) => (
           <ArticleCard
             id={id}
-            image={`${API_URL}${articleImage.url}`}
+            image={`${articleImage.url}`}
             title={title}
             author={author}
             date={date}
