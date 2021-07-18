de1c646cbe690f84a2f03f1514e4364af4803563
narimanadam <narimanadam7@gmail.com>
2021-07-19 00:11:13 +0400

    fix google signin url

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -22,7 +22,7 @@ const options = {
       const isSignIn = user ? true : false;
       if (isSignIn) {
         const response = await fetch(
-          `${API_URL}/auth/${account.provider}/callback?access_token=${account?.accessToken}`
+          `${process.env.API_URL}/auth/${account.provider}/callback?access_token=${account?.accessToken}`
         );
         const data = await response.json();
         token.jwt = data.jwt;
