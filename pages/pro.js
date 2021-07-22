267160eacb89fc0a83464ee77051397b2bf5e7d4
narimanadam <narimanadam7@gmail.com>
2021-07-23 03:16:51 +0400

    fix getinitialprops

--- pages/pro.js
+++ pages/pro.js
@@ -18,7 +18,7 @@ const Pro = ({ articles }) => {
   );
 };
 
-export async function getStaticProps({ req }) {
+export async function getServerSideProps({ req }) {
   const jwt = req.cookies.jwt || "";
 
   const { API_URL } = process.env;
