15799d1746d3ea8761e0ddc080ee9feff068a837
narimanadam <narimanadam7@gmail.com>
2021-07-23 20:40:45 +0400

    fix articles

--- pages/pro.js
+++ pages/pro.js
@@ -3,14 +3,12 @@ import cookies from "next-cookies";
 import useTranslation from "next-translate/useTranslation";
 import { getSession } from "next-auth/client";
 
-const Pro = ({ articles, jwt }) => {
+const Pro = ({ articles }) => {
   const { t } = useTranslation("common");
 
   return (
     <div className="container mx-auto">
       <h1 className="text-white">{t("ifYouSee")}</h1>
-      {console.log("Arrrr", articles)}
-      {console.log("jwttttqqqq", jwt)}
       {articles?.articles?.map(({ title, body }) => (
         <div key={title}>
           <h2 className="text-white text-xl font-bold mb-3">{title}</h2>
@@ -38,7 +36,6 @@ export async function getServerSideProps(ctx) {
   return {
     props: {
       articles,
-      jwt,
     },
   };
 }
