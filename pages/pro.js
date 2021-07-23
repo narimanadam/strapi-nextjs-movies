6dd42db4018857b8b67fd7cb779c780bc4b9f190
narimanadam <narimanadam7@gmail.com>
2021-07-23 20:04:33 +0400

    fix pro

--- pages/pro.js
+++ pages/pro.js
@@ -8,7 +8,7 @@ const Pro = ({ articles }) => {
     <div className="container mx-auto">
       <h1 className="text-white">{t("ifYouSee")}</h1>
       {console.log("Arrrr", articles)}
-      {articles.articles.map(({ title, body }) => (
+      {articles?.articles.map(({ title, body }) => (
         <div key={title}>
           <h2 className="text-white text-xl font-bold mb-3">{title}</h2>
           <span className="text-white">{body}</span>
